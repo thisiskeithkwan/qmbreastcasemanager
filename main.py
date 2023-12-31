ac52cf83f0cbce0aff70c5243d781d36c0ffd2b1
thisiskeithkwa1 <26728654-thisiskeithkwa1@users.noreply.replit.com>
2023-12-31 03:34:36 +0000

    new

--- main.py
+++ main.py
@@ -4,11 +4,12 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
+
 @app.get("/")
 def read_root():
-    return {"Hello": "World"}
+  return {"Hello": "World"}
 
 
 @app.get("/items/{item_id}")
 def read_item(item_id: int, q: Union[str, None] = None):
-    return {"item_id": item_id, "q": q}
+  return {"item_id": item_id, "q": q}
